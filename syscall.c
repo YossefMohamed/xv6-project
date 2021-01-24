5216cd42ebfd17e488328f99f88edeb7303401bb
hasnaazaki123 <76954101+hasnaazaki123@users.noreply.github.com>
2021-01-24 23:16:14 +0200

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -103,7 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-
+extern int sys_settickets(void);
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
 [SYS_exit]    sys_exit,
@@ -126,6 +126,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+  [SYS_settickets]  sys_settickets,
 };
 
 void

88fc23a15c23c689100511af6095902f214a721e
hasnaazaki123 <76954101+hasnaazaki123@users.noreply.github.com>
2021-01-24 23:23:45 +0200

    Update defs.h

--- defs.h
+++ defs.h
@@ -120,7 +120,8 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
-
+int             settickets(int);
+int             lottery_Total(void);
 // swtch.S
 void            swtch(struct context**, struct context*);
 
